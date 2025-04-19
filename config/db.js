8c993edf8c4b9c6ba758b54f70cee67e91d8d203
kunda2575 <kunda2575@gmail.com>
2025-04-19 08:17:54 +0530

    package change

--- config/db.js
+++ config/db.js
@@ -10,12 +10,23 @@ dotenv.config();
 // });
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASS,
+
+
+  process.env.DB_URL,
   {
-    host: process.env.DB_HOST,
+    // host: process.env.DB_HOST,
     dialect: "postgres",
+
+dialectOptions:{
+  ssl:{
+    require:true,
+    rejectUnauthorized:false
+  }
+},
+
     logging: console.log, // ✅ Enables SQL query logging
     pool: {
       max: 10,
