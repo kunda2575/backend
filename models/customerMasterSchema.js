87736b5b75be3754abaa4ddb6f7b7669d230d1d7
kunda2575 <kunda2575@gmail.com>
2025-04-25 18:48:32 +0530

    all tables change in customer

--- models/customerMasterSchema.js
+++ models/customerMasterSchema.js
@@ -11,7 +11,7 @@ const CustomerMaster = sequelize.define(
             primaryKey: true
         },
         customerId:{
-            type:DataTypes.TEXT,
+            type:DataTypes.STRING,
             allowNull :false,
             unique: true
         },
